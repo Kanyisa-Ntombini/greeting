fe7af451c13a6ff4e91c213f916ea526eddf66fa
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2021-05-24 23:40:56 +0200

    Fixed some unit tests

--- unit-testing/testing.js
+++ unit-testing/testing.js
@@ -1,5 +1,17 @@
 describe('Greetings function:' , 
     function() {
+        it ('should check if a name is entered', 
+            function () {
+                let testGreeting = GreetMe();
+                testGreeting.setName('');
+                assert.deepEqual(false, testGreeting.checkName());
+
+                let testGreeting2 = GreetMe();
+                testGreeting2.setName('Grapes');
+                assert.deepEqual(true, testGreeting2.checkName());
+            }
+        );
+
         it ('should check if the right name is stored', 
             function () {
                 let testGreeting = GreetMe();
@@ -21,8 +33,8 @@ describe('Greetings function:' ,
         it ('should ensure only the first letter is in uppercase', 
             function () {
                 let testGreeting = GreetMe();
-                testGreeting.setName('autUMn');
-
+                testGreeting.setName('autUMn'.toLocaleLowerCase());
+            
                 assert.deepEqual('Autumn', testGreeting.getName());
             }
         );
@@ -30,11 +42,16 @@ describe('Greetings function:' ,
         it ('should check if a language was selected', 
             function () {
                 let testGreeting = GreetMe();
-                assert.deepEqual(true, testGreeting.checkLanguage('samoa'));
+                testGreeting.setLang('samoa');
+                assert.deepEqual(true, testGreeting.checkLanguage());
 
                 let testGreeting2 = GreetMe();
-                assert.deepEqual(false, testGreeting2.checkLanguage(null));
+                testGreeting2.setLang();
+                assert.deepEqual(false, testGreeting2.checkLanguage());
             }
         );
+
+        //Test for greeting
+        //Test for counter
     }
 );
